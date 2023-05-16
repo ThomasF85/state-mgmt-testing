acdf585b4139682ff788c3eaedb71b0052a293e4
ThomasF85 <thomas.foeldi@gmail.com>
2023-05-16 09:33:49 +0200

    add test

--- components/CounterValue.js
+++ components/CounterValue.js
@@ -4,12 +4,12 @@ const Value = styled.p`
   background-color: var(--color-primary);
   font-size: 1.2em;
   font-weight: 700;
-  width: 2em;
-  height: 2em;
-  line-height: 2em;
+  width: 5em;
+  height: 5em;
+  line-height: 5em;
   border-radius: 50%;
 `;
 
 export default function CounterValue({ value }) {
-  return <Value>{value}</Value>;
+  return <Value>Value: {value}</Value>;
 }
